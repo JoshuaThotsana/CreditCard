de8481807bfe1b3e7a763d15304ab4208f9817bf
Thotsana <thotsana.mabotsa@umuzi.org>
2019-12-14 14:10:53 +0200

    second commit

---
+++ src/CreditCard.java
@@ -0,0 +1,63 @@
+public class CreditCard {
+    //Instance variables:
+    private String customer;                // name of the customer (e.g., "Joshua Mabotsa")
+    private String bank;                    // name of the bank (e.g., "Standard Bank")
+    private String account;                 // account identifier (e.g., "5391 0375 9387 5309")
+    private int limit;                      // credit limit (measured in Rands)
+    protected double balance;               // current balance (measured in Rands)
+    // Constructors:
+    /**
+     * Constructs a new credit card instance.
+     * @param customer      the name of the customer (e.g, "Joshua Mabotsa)
+     * @param bank          the name of the bank (e.g., "Standard Bank")
+     * @param account       the account identifier (e.g, "5391 0375 9387 5309")
+     * @param limit         the credit limit (measured in Rands)
+     * @param balance       the initial balance (measured in Rands)
+     */
+    public CreditCard(String customer, String bank, String account, int limit, double balance) {
+        this.customer = customer;
+        this.bank = bank;
+        this.account = account;
+        this.limit = limit;
+        this.balance = balance;
+    }
+    public CreditCard(String customer, String bank, String account, int limit) {
+        this(customer,bank,account,limit,0.0);              // use a balance of zero as default.
+    }
+    // Accessor methods:
+    public String getCustomer() { return customer;}
+    public String getBank() {return bank;}
+    public String getAccount() {return account;}
+    public int getLimit() {return limit;}
+    public double getBalance() {return balance;}
+    // Update methods:
+
+    /**
+     * Charges the given price to the card, assuming sufficient credit limit.
+     * @param price the amount to be charged
+     * @return true if charge was accepted; false if charge was denied
+     */
+    public boolean charge(double price) {                           // make a charge
+        if (price + balance > limit) {                              // if charge would surpass limit
+            return false;                                           // refuse the charge
+        } // at this point the charge is successful.
+        balance += price;                                           // update the balance
+        return true;                                                // announce the good news
+    }
+
+    /**
+     * Processes customer payment that reduces balance.
+     * @param amount
+     */
+    public void makePayment(double amount) {                        // make a payment
+        balance -= amount;
+    }
+    // Utility method to print a card's information
+    public static void printSummary(CreditCard card) {
+        System.out.println("Customer = " + card.customer);
+        System.out.println("Bank = " + card.bank);
+        System.out.println("Account = " + card.account);
+        System.out.println("Balance = " + card.balance);            // implicit cast
+        System.out.println("Limit = " + card.limit);                // implicit cast
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+/**
+ * A simple model for a consumer credit card.
+ *
+ * @author Thotsana Joshua Mabotsa.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        CreditCard[] wallet = new CreditCard[3];
+        wallet[0] = new CreditCard("Joshua Mabotsa", "Standard Bank", "5391 0375 9387 5309", 5000);
+        wallet[1] = new CreditCard("Joshua Mabotsa", "ABSA", "3485 0399 3395 1954", 3500);
+        wallet[2] = new CreditCard("Joshua Mabotsa", "FNB", "5391 0375 9387 5309", 2500, 300);
+
+        for (int val = 1; val <= 16; val++) {
+            wallet[0].charge(3*val);
+            wallet[1].charge(2*val);
+            wallet[2].charge(val);
+        }
+
+        for (CreditCard card : wallet) {
+            CreditCard.printSummary(card);              // calling static method
+            while (card.getBalance() > 200.0) {
+                card.makePayment(200);
+                System.out.println("New balance = " + card.getBalance());
+            }
+        }
+    }
+
+}
